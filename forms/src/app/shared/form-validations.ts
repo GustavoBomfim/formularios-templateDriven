59a759e1f08792f1b61d8a68d4e81e90f0d82a75
GustavoBomfim <gustavosoaresbomfim@hotmail.com>
2022-02-16 15:44:54 -0300

    Validação entre dois campos (confirmar email)

--- forms/src/app/shared/form-validations.ts
+++ forms/src/app/shared/form-validations.ts
@@ -1,4 +1,4 @@
-import { AbstractControl, FormArray, FormControl } from '@angular/forms';
+import { AbstractControl, FormArray, FormControl, FormGroup } from '@angular/forms';
 
 export class FormValidations {
 
@@ -21,5 +21,30 @@ export class FormValidations {
             return validacep.test(cep) ? null : { cepInvalido: true };
         }
         return null;
+    }
+
+    static equalsTo(otherField: string){
+        const validator = (formControl: FormControl) => {
+            if (otherField == null){
+                throw new Error('É necessário informar um campo.');
+            }
+
+            if(!formControl.root || !(<FormGroup>formControl.root).controls){
+                return null;
+            }
+           
+
+            const field = (<FormGroup>formControl.root).get(otherField);
+
+            if (!field){
+                throw new Error('É necessário informar um campo válido.');
+            }
+
+            if(field.value !== formControl.value){
+                return { equalsTo: otherField};
+            }
+            return null;
+        };
+        return validator;
     }
 } 
